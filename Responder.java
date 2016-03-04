20d6cd5aa16328b4e8400e84dff5463513c8155a
Alvaro <alvaroperbay97@gmail.com>
2016-03-04 11:49:35 +0100

    Añadida a la clase responder un constructor y modificado el metodo para que devuelva varias respuestas aleatorias

--- Responder.java
+++ Responder.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.ArrayList;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +9,23 @@
  */
 public class Responder
 {
+    //Almacena las respuestas
+    private ArrayList<String> respuestas;
+    //Numero aleatorio
+    private Random aleatorio;
+    
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        respuestas = new ArrayList<String>();
+        respuestas.add("Ok, cual es el problema");
+        respuestas.add("Expliquese con mas claridad");
+        respuestas.add("Ahora mismo le pasamos con un tecnico");
+        respuestas.add("Espere un momento... Gracias");
+        respuestas.add("¿Quiere contratar alguna tarifa?");
+        aleatorio = new Random();
     }
 
     /**
@@ -20,6 +34,7 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int numRespuesta = aleatorio.nextInt(respuestas.size());
+        return respuestas.get(numRespuesta);
     }
 }
